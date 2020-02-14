b4ce1ff85480591de74a777c0d85c3a38766a1d0
goukey <44106685+goukey@users.noreply.github.com>
2020-02-14 18:45:09 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -1,7 +1,9 @@
+
 #!/bin/bash
 
-blue(){
-    echo -e "\033[34m\033[01m$1\033[0m"
+#fonts color
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
 }
 green(){
     echo -e "\033[32m\033[01m$1\033[0m"
